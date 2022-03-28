267ebb2fdf3f7de3b3c51cd52a4398b70d2540e5
Heiszer Marta <88995174+Heiszer@users.noreply.github.com>
2022-03-28 07:37:50 +0700

    Add files via upload

---
+++ fundamental.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    
+    int a = 2;
+
+    cout << sizeof(a) << endl;
+    cout << numeric_limits<int>::max() << endl;
+    cin.get();
+    return 0;
+}
